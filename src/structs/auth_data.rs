37127145908719c3900a313ad2d3cb8e278c7cb2
tverghis <tarun.verghis@gmail.com>
2018-05-02 07:39:58 -0500

    Remove references to multiple tokens in documentation

--- src/structs/auth_data.rs
+++ src/structs/auth_data.rs
@@ -1,14 +1,12 @@
 use serializer::{Serializer, Serializable, FieldType};
 
-/// Represents authentication tokens that can be sent to the endpoint to validate requests.
+/// Represents an authentication token that can be sent to the endpoint to validate requests.
 ///
-/// In LAN settings, these tokens are probably not needed.
-/// If sending requests in non-LAN settings, consider using SSL to protect the integrity of these tokens.
+/// In LAN settings, the token is probably not needed.
+/// If sending requests in non-LAN settings, consider using SSL to protect the integrity of the token.
 pub struct AuthData {
 
     /// A token that can be used to authenticate requests to the endpoint.
-    ///
-    /// **NOTE:** This should be a `Vec<String>` to support multiple tokens.
     pub token: String
 }
 
